ca3b9136fc3ac43d8764e3226ae452a61078345f
The Real Tech Lady <bukolaarowosebi@gmail.com>
2022-02-16 09:58:25 +0100

    home work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {useState} from 'react';
+
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList,
+  IonItem, IonLabel, IonInput, IonButton, IonAlert, useIonAlert
+ } from '@ionic/react';
+
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [login, setLogin] = useIonAlert();
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+    <IonPage >
+      <IonContent >
+        <IonList className='page'>
+          <IonItem>
+            <IonLabel position='floating'>Email: </IonLabel>
+            <IonInput placeholder='example@gmail.com' type='text'  />
+          </IonItem>
+
+          <IonItem>
+            <IonLabel position='floating'>Password: </IonLabel>
+            <IonInput placeholder='*********'  type='password' />
+          </IonItem>
+
+        </IonList>
+
+         <div className='btnContainer'>
+        <IonButton onClick={() => login('Login Complete', [{text: 'ok'}])}>Log In</IonButton>
+        <IonButton color='danger'>Sign Up</IonButton>
+        </div>
+
+        <p className='password'>Forgot Password?</p>
+
+        <div className='btnContainer2'>
+          <IonButton color='danger'>Log In with Google</IonButton>
+          <IonButton>Log In with Facebook</IonButton>
+        </div>
       </IonContent>
+      
     </IonPage>
   );
 };
